d282202aa968cd39345ed297688ab0d9c2706ccc
zhouhang <zhouhang@zhilai.com>
2019-03-28 11:27:13 +0800

    add install tutorial

--- src/main/resources/add_vir_mac.sh
+++ src/main/resources/add_vir_mac.sh
@@ -1,15 +1,22 @@
-ETH_TMP=`ifconfig`
-ETH_TMP=${ETH_TMP%% *}
-echo "eth name is: $ETH_TMP"
+ETH_TMP=`ifconfig en0`
+ETH_TMP=${ETH_TMP%%:*}
+if [ $ETH_TMP != "en0" ]; then
+	echo "Don't have en0, please change to the right ethernet!"
+	exit -1;
+fi
+
+#please change ETH_TMP to your own ethernet card
+ETH_TMP=en0
 
 if [ -z $ETH_TMP ]; then
-        ETH_TMP=lo0
+        ETH_TMP=en0
 fi
 
 VIR_ETH_COUNT=$#
 if [ $VIR_ETH_COUNT -eq 0 ]; then
-        echo "Will not add virtual eth"        
-        exit 0;
+        echo "Useage:"
+	echo "       ./add_vir_mac.sh 192.168.1.101 192.168.1.102 ..."        
+        exit -1;
 fi
 
 TMP_COUNT=0
@@ -17,7 +24,7 @@ for i in $*;
 do
 
         echo "add a new virtual: $NEW_VIR_ETH, ip is $i"
-        sudo ifconfig lo0 alias $i 255.255.255.0
+        sudo ifconfig $ETH_TMP alias $i 192.168.1.255
         let TMP_COUNT+=1
 done
 exit 0;
--- src/main/resources/del_vir_mac.sh
+++ src/main/resources/del_vir_mac.sh
@@ -1,5 +1,6 @@
-ETH_TMP=`ifconfig`
-ETH_TMP=${ETH_TMP%% *}
+#ETH_TMP=`ifconfig`
+#ETH_TMP=${ETH_TMP%% *}
+ETH_TMP=en0
 echo "eth name is: $ETH_TMP"
 
 if [ -z $ETH_TMP ]; then
@@ -17,7 +18,7 @@ for i in $*;
 do
 
         echo "del gived virtual network: ip is $i"
-        sudo ifconfig lo0 -alias $i
+        sudo ifconfig $ETH_TMP -alias $i
         let TMP_COUNT+=1
 done
 exit 0;
